9ba3fbabc15106000e124fd72ae94fa2d1bb11b0
prestonbrubaker <brubakerpreston@gmail.com>
2026-05-01 22:42:56 -0500

    m

--- main.c
+++ main.c
@@ -1,4 +1,23 @@
 //hello
-int main(){
+#include <stdio.h>
+#include <stdlib.h>
 
+int write_to_file(){
+    const char *filename = "output.txt";
+    const char *message = "Hello World!";
+    // Write to file
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
+        perror("Error opening file");
+        return EXIT_FAILURE;
+    }
+    fprintf(file, "%s", message);
+    fclose(file);
+    return EXIT_SUCCESS;
+}
+
+
+int main(){
+    write_to_file();
+    printf("File Complete!\n");
 }
